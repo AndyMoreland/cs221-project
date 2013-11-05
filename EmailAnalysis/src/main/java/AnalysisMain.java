baa74f4e5ed52fe3059cc9d6fe795b769123d10c
Leo Martel <lmartel@github>
2013-11-05 01:56:15 -0800

    splitting data

--- EmailAnalysis/src/main/java/AnalysisMain.java
+++ EmailAnalysis/src/main/java/AnalysisMain.java
@@ -1,5 +1,7 @@
 import java.sql.*;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class AnalysisMain {
@@ -9,10 +11,23 @@ public class AnalysisMain {
         ResultSet emailResults = statement.executeQuery("SELECT * FROM emails");
         Classifier simpleClassifier = new SimpleClassifier(connection, "andymo@stanford.edu");
 
-        List<Email> emails = Email.parseEmails(emailResults);
+        List<Email> training = new ArrayList<Email>();
+        List<Email> test = new ArrayList<Email>();
+        splitData(Email.parseEmails(emailResults), training, test);
 
-        for (Email email : emails) {
+        for (Email email : training) {
             System.out.println(simpleClassifier.classify(email));
+        }
+    }
+
+    private static void splitData(List<Email> all, List<Email> training, List<Email> test){
+        Collections.shuffle(all);
+        int i = 0;
+        for(; i < all.size() * 0.7; i++){
+            training.add(all.get(i));
+        }
+        for(; i < all.size(); i++){
+            test.add(all.get(i));
         }
     }
 }
